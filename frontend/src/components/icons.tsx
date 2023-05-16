0f1ad371523eafb75e904103e483cd5ef50f9355
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-05-16 18:28:19 +0700

    add frontend

---
+++ frontend/src/components/icons.tsx
@@ -0,0 +1,151 @@
+import { component$ } from "@builder.io/qwik";
+
+interface IconProps {
+  class: string;
+}
+
+export const HomeIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+      />
+    </svg>
+  );
+});
+
+export const UsersIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+      />
+    </svg>
+  );
+});
+
+export const AccountIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+      />
+    </svg>
+  );
+});
+
+export const ArchiveXIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+      />
+    </svg>
+  );
+});
+
+export const ArchiveIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+      />
+    </svg>
+  );
+});
+
+export const StackIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122"
+      />
+    </svg>
+  );
+});
+
+export const LogoutIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+      />
+    </svg>
+  );
+});
+
+export const LoveIcon = component$<IconProps>((props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+    </svg>
+  );
+});
---
+++ frontend/src/routes/layout.tsx
@@ -0,0 +1,125 @@
+import { component$, Slot } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import {
+  AccountIcon,
+  ArchiveIcon,
+  ArchiveXIcon,
+  HomeIcon,
+  LogoutIcon,
+  LoveIcon,
+  StackIcon,
+  UsersIcon,
+} from "~/components/icons";
+
+export default component$(() => {
+  const menus: Array<{
+    icon: any;
+    title: string;
+    path: string;
+  }> = [
+    {
+      icon: HomeIcon,
+      title: "Dashboard",
+      path: "/",
+    },
+    {
+      icon: UsersIcon,
+      title: "Data User",
+      path: "/user",
+    },
+    {
+      icon: ArchiveXIcon,
+      title: "Data Barang Hilang",
+      path: "/barang-hilang",
+    },
+    {
+      icon: ArchiveIcon,
+      title: "Data Barang Temuan",
+      path: "/barang-temuan",
+    },
+    {
+      icon: StackIcon,
+      title: "Testimoni",
+      path: "/testimoni",
+    },
+    {
+      icon: AccountIcon,
+      title: "Akun",
+      path: "/akun",
+    },
+  ];
+
+  return (
+    <div class="bg-gray-100 min-h-screen text-gray-700">
+      <div class="z-5 bg-black bg-opacity-25 fixed top-0 left-0 right-0 bottom-0 lg:hidden"></div>
+      <div class="fixed top-0 bottom-0 left-0 w-80 bg-white z-5 p-10 px-8 flex flex-col justify-between">
+        <div>
+          <div class="text-4xl font-extrabold">
+            LostInfo<span class="text-green-500">.</span>
+          </div>
+          <div class="text-sm text-gray-300">
+            Platform Informasi Barang Hilang
+          </div>
+          <div class="mt-10 -mx-8">
+            <ul>
+              {menus.map((item, index) => (
+                <li class="mb-1 relative px-8" key={"menu" + index}>
+                  <Link
+                    href={item.path}
+                    class={[
+                      "p-2 px-3 block rounded flex items-center",
+                      item.path == "/"
+                        ? "text-green-600 mb-1 bg-green-200"
+                        : "",
+                    ]}
+                  >
+                    <item.icon class="w-5 h-5 mr-3" />
+                    {item.title}
+                  </Link>
+                  {item.path == "/" && (
+                    <div class="h-full w-1 bg-green-500 rounded-r absolute top-0 left-0 bottom-0"></div>
+                  )}
+                </li>
+              ))}
+              <li class="mb-1 relative px-8">
+                <a
+                  href="#"
+                  class="p-2 px-3 block rounded flex items-center space-x-3"
+                >
+                  <LogoutIcon class="w-5 h-5 mr-3" />
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div class="text-sm">
+            Made with <LoveIcon class="w-4 h-4 text-red-500 inline" /> by
+            HockGuan
+          </div>
+          <div class="font-semibold mt-2 text-sm">
+            LostInfo - 2023 &copy; All rights reserved.
+          </div>
+        </div>
+      </div>
+      <div class="lg:ml-80 p-5">
+        <div class="flex justify-between mb-5 items-center">
+          <input
+            type="text"
+            class="bg-white rounded p-2.5 px-4 outline-none flex-1"
+            placeholder="Cari..."
+          />
+          <div class="flex-1 flex justify-between items-center">
+            <div class="flex-1"></div>
+            <div class="flex-1 items-center flex justify-end space-x-4 border-l-2">
+              <div>Hello, Superadmin</div>
+              <div class="rounded-full w-12 h-12 bg-gray-200"></div>
+            </div>
+          </div>
+        </div>
+        <Slot />
+      </div>
+    </div>
+  );
+});
