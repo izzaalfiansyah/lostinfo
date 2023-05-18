88a0f6a9bcabdc2c4810f8f4a7d470090a727d51
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-05-18 23:58:40 +0700

    add function to generate base64 upload

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,8 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Nette\Utils\Random;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function uploadBase64($encode, $path, $extension)
+    {
+        $file = explode(';base64,', $encode)[1];
+        $filename = Random::generate(20) . '.' . $extension;
+        $filepath = $path . '/' . $filename;
+
+        file_put_contents(public_path($filepath), base64_decode($file));
+        return $filepath;
+    }
 }
