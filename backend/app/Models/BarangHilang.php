fe76d7b65f4a11d62df4a8ac9ea8d82517e06d06
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-06-22 20:00:18 +0700

    make barang hilang table

---
+++ backend/app/Models/BarangHilang.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangHilang extends Model
+{
+    use HasFactory;
+
+    public $table = 'barang_hilang';
+
+    public $fillable = [
+        'user_id',
+        'nama',
+        'deskripsi',
+        'tempat_hilang',
+        'maps',
+        'foto',
+        'hadiah',
+        'ditemukan',
+    ];
+
+    public $cast = [
+        'user_id' => 'integer',
+        'foto' => 'object',
+        'hadiah' => 'integer'
+    ];
+}
