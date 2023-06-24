9fb0aa4cd71a383b59f25f9376f86a76dcf84650
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-06-24 14:26:17 +0700

    get id pengirim from deetail chat

--- backend/app/Http/Resources/ChatDetailResource.php
+++ backend/app/Http/Resources/ChatDetailResource.php
@@ -15,15 +15,24 @@ class ChatDetailResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $chat = Chat::find($this->chat_id);
+
+        if ($this->pengirim == '1') {
+            $pengirim_id = $chat->user_1_id;
+        } else {
+            $pengirim_id = $chat->user_2_id;
+        }
+
         return [
             'id' => $this->id,
             'chat_id' => $this->chat_id,
-            'chat' => new ChatResource(Chat::find($this->chat_id)),
+            'chat' => new ChatResource($chat),
             'pesan' => $this->pesan,
             'maps' => $this->maps,
             'foto' => $this->foto,
             'foto_url' => $this->foto ? asset($this->foto) : '',
             'pengirim' => $this->pengirim,
+            'pengirim_id' => $pengirim_id,
             'dibaca' => (int) $this->dibaca,
             'dibaca_detail' => ['Belum', 'Sudah'][(int) $this->dibaca],
         ];
