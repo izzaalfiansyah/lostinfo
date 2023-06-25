3bdbaa005a629bb62273bf22fb7b08413b5bf9e3
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-06-26 01:58:19 +0700

    add z-index to notification

--- frontend/src/contexts/notif.tsx
+++ frontend/src/contexts/notif.tsx
@@ -10,7 +10,7 @@ export default component$<{
   color: string;
 }>((props) => {
   return (
-    <div class={["fixed bottom-5 left-5 right-5 z-40 flex justify-center"]}>
+    <div class={["fixed bottom-5 left-5 right-5 z-[50] flex justify-center"]}>
       <div
         class={[
           "rounded-lg text-white p-4 px-5 max-w-full w-[800px] transform transition duration-500",
