918b37924d9493db52508071b392bda661e6a2a7
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-07-10 10:14:32 +0700

    revisi notification

--- frontend/src/contexts/notif.tsx
+++ frontend/src/contexts/notif.tsx
@@ -10,12 +10,16 @@ export default component$<{
   color: string;
 }>((props) => {
   return (
-    <div class={["fixed bottom-5 left-5 right-5 z-[50] flex justify-center"]}>
+    <div
+      class={[
+        "fixed bottom-5 left-5 right-5 z-[50] flex justify-center transform transition duration-500",
+        props.show ? "translate-y-0" : "translate-y-[100px]",
+      ]}
+    >
       <div
         class={[
-          "rounded-lg text-white p-4 px-5 max-w-full w-[800px] transform transition duration-500",
+          "rounded-lg text-white p-4 px-5 max-w-full w-[800px]",
           props.color,
-          props.show ? "translate-y-0" : "translate-y-[100px]",
         ]}
       >
         {props.message}
