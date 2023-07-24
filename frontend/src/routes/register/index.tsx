43321742a144e5373a6d8cc1979266b0d4ae8b1e
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-07-24 20:03:27 +0700

    make privacy

--- frontend/src/routes/register/index.tsx
+++ frontend/src/routes/register/index.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { For, createSignal, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 import { A, useNavigate } from "solid-start";
 import Button from "~/components/button";
@@ -24,8 +24,19 @@ export default function () {
   const [isAgree, setIsAgree] = createSignal(false);
   const [isLoading, setIsLoading] = createSignal(false);
 
+  const [privacy, setPrivacy] = createSignal<string[]>([]);
+
   const notif = useNotif();
   const nav = useNavigate();
+
+  const getPrivacy = async () => {
+    try {
+      const { data } = await http.get("/privacy");
+      setPrivacy(data.data);
+    } catch (e: any) {
+      notif.show(e.response.data.message, false);
+    }
+  };
 
   const handleKTPChange = async (e: any) => {
     const file = e.target.files[0];
@@ -62,6 +73,10 @@ export default function () {
     }
     setIsLoading(false);
   };
+
+  onMount(() => {
+    getPrivacy();
+  });
 
   return (
     <div class="max-w-full w-[900px]">
@@ -206,60 +221,7 @@ export default function () {
         <div class="max-w-full w-[600px]">
           <div class="pl-5">
             <ul class="list-disc text-sm">
-              <li>
-                Pengguna dengan ini menyatakan bahwa pengguna adalah orang yang
-                cakap dan mampu untuk mengikatkan dirinya dalam sebuah
-                perjanjian yang sah menurut hukum. Pengguna yang tidak cakap dan
-                mampu mengikatkan dirinya dalam sebuah perjanjian yang sah
-                menurut hukum dalam mengakses Situs, menggunakan Situs dan/atau
-                melakukan pendaftaran akun melalui situs LostInfo, atau
-                melakukan aktivitas lain di Situs, dengan ini menyatakan bahwa
-                seluruh tindakan-tindakan tersebut dilakukan dalam
-                sepengetahuan, pengawasan dan persetujuan yang sah dari orang
-                tua atau wali atau pengampu pengguna bagi yang berusia dibawah
-                17 tahun.
-              </li>
-              <li>
-                Sebelum menggunakan Situs, Pengguna menyetujui Syarat &
-                Ketentuan ini dan Kebijakan Privasi.
-              </li>
-              <li>
-                Untuk dapat melakukan Pendaftaran Akun LostInfo, Pengguna harus
-                melakukan pendaftaran Akun terlebih dahulu pada Situs. Untuk
-                menghindari keraguan, Manajemen Pelaksana tidak bekerja sama
-                dengan pihak ketiga manapun dalam penyelenggaraan Pendaftaran
-                Akun LostInfo.
-              </li>
-              <li>
-                Pendaftaran Akun harus menggunakan email Pengguna yang masih
-                aktif dan dilakukan verifikasi pada saat mendaftar.
-              </li>
-              <li>
-                Dalam melakukan pendaftaran Akun, Pengguna wajib memasukkan data
-                diri Pengguna yang meliputi, paling sedikit, (i) nama lengkap,
-                (ii) alamat, (iii) email, (iv) telepon, (v) foto KTP, (vi)
-                username, dan (vii) password atas nama Pengguna.
-              </li>
-              <li>
-                Pengguna wajib mengisi dan/atau memberikan data atau informasi
-                pada Situs dengan benar, dan Pengguna dilarang memberikan data
-                atau informasi yang tidak benar dan/atau melakukan manipulasi
-                data dan/atau pemalsuan data. Pengguna memahami bahwa pemberian
-                data atau informasi yang tidak benar dapat dianggap sebagai
-                suatu perbuatan melawan hukum yang dapat menimbulkan konsekuensi
-                hukum, baik secara perdata dan/atau pidana sebagaimana diatur
-                dalam, termasuk namun tidak terbatas pada, Kitab Undang-Undang
-                Hukum Pidana dan Undang-Undang Nomor 11 Tahun 2008 tentang
-                Informasi dan Transaksi Elektronik.
-              </li>
-              <li>
-                Pengguna wajib bertanggungjawab atas barang temuan dengan
-                bersungguh-sungguh. Pengguna memahami bahwa pertanggungjawaban
-                atas barang temuan telah di atur di sisi hukum, tindakan
-                mengambil barang yang ditemukan di tengah jalan untuk dimiliki,
-                bisa dijerat Pasal 372 KUHP (tindak pidana penggelapan) atau
-                Pasal 362 KUHP (tindak pidana pencurian).
-              </li>
+              <For each={privacy()}>{(item) => <li>{item}</li>}</For>
             </ul>
             <div class="text-sm mt-5">
               <Checkbox
